69a26635ffce85456551c0e2d708a6f51f5adf5d
jason-mac <jason_hh@outlook.com>
2023-07-16 19:34:31 -0400

    update

--- bot.py
+++ bot.py
@@ -1,15 +1,49 @@
+# encoding: utf-8
+
 # import needed libraries
+from traderlib import *
+from logger import *
+import sys
 
-# initialize the logger
-
-# check our trading account (blocked? total amount?)
+# check our trading account
+def check_account_ok():
+    try:
+        # get account info
+    except Exception as e :
+        lg.error('Could not get account info')
+        lg.info(str(e))
+        sys.exit()
 
 # close current orders (double check)
+def clean_open_orders():
+    # open_orders = list of open orders
+    lg.info('List of open orders')
+    lg.info(str(open_orders))
 
-# define asset (e.g. TSLA)
-    # IN: keyboard
-    # OUT: string
+    for order in open_orders:
+        # close order
+        lg.info('Order %s closed' % str(order.id))
+
+    lg.info('Closing orders complete')
 
 # execute trading bot
-    # IN: string (ticker)
-    # OUT: boolean (True = success / False = failure)
+def main():
+
+    # initialize the logger
+    initialize_logger()
+
+    # check our trading account
+    check_account_ok()
+
+    # close current orders
+    clean_open_orders()
+
+    # define asset
+    ticker = input('Write the ticker you want to operate with: ')
+
+    trader = Trader(ticker) # initialize trading bot
+    trader.run() # run trading bot
+        # OUT: boolean (True = success / False = failure)
+
+if __name__ == '__main__':
+    main()
--- logger.py
+++ logger.py
@@ -1,30 +1,28 @@
-
-
+# encoding: utf-8
 
 import logging as lg
 import os
 from datetime import datetime
 
-# creating a folder for the logs
-logs_path = './logs/'
-try:
-    os.mkdir(logs_path)
-except OSError:
-    print("Creation of the directory %s failed - it does not have to be bad" % logs_path) 
-else:
-    print("succesfully created log directory")
+def initialize_logger():        
 
-# renaming each log depending on the time
+    # creating a folder for the logs
+    logs_path = './logs/'
+    try:
+        os.mkdir(logs_path)
+    except OSError:
+        print("Creation of the directory %s failed - it does not have to be bad" % logs_path) 
+    else:
+        print("succesfully created log directory")
 
-date = datetime.now().strftime("%Y%m%d_%H%M%S")
-log_name = date + ".log"
-currentLog_path = logs_path + log_name
+    # renaming each log depending on the time
+    date = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_name = date + ".log"
+    currentLog_path = logs_path + log_name
 
-lg.basicConfig(filename=currentLog_path, format='%(asctime)s - %(levelname)s: %(message)s', level=lg.DEBUG)
-lg.getLogger().addHandler(lg.StreamHandler())
-# logging levels: DEBUG, INFO, WARNING, ERROR
-
-lg.info('This is an info message!')
-lg.debug('This is a debugging message!')
-lg.warning('This is a warning message.')
-lg.error('This is an error message!')
+    # log parameters
+    lg.basicConfig(filename=currentLog_path, format='%(asctime)s - %(levelname)s: %(message)s', level=lg.DEBUG)
+    lg.getLogger().addHandler(lg.StreamHandler())
+    
+    # init message
+    lg.info('Log initialized')
